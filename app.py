7ca37ece3a55a4a38476458d397f2c3a5882c5b3
EricHenry <correia.eh@gmail.com>
2017-06-05 00:06:33 -0400

    create weather API

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+import requests
 
 app = Flask(__name__)
 
+@app.route('/weather')
+def index():
+    r = requests.get('https://api.darksky.net/forecast//41.6362,70.9342')
+    print(r.json());
+    return jsonify(results=r.json())
+
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
